235ea2df90a4980315e92d8eaeea263717aff9ad
Winime Awinbugur Azumah <winimeawinbugura@gmail.com>
2025-11-08 12:29:05 +0000

    Script that prints two arguments

---
+++ 4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+// 4-concat.js
+// Script that prints two arguments in the format "<arg1> is <arg2>"
+
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+console.log(`${firstArg} is ${secondArg}`);
