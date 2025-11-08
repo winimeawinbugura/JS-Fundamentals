8cc72f3beab63e149b456aca9db649c9bb78739c
Winime Awinbugur Azumah <winimeawinbugura@gmail.com>
2025-11-08 13:21:46 +0000

    Script that prints 3 lines using an array and a loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+// 6-multi_languages_loop.js
+// Script that prints 3 lines using an array and a loop
+
+const languages = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
